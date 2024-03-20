e308bcbbf6aa20ff6f4b445bcb70f578782c4cfd
xinxinchu <144758431+xinxinchu@users.noreply.github.com>
2024-03-20 11:53:44 +0800

    week05

---
+++ week05/week05-1a.py
@@ -0,0 +1,2 @@
+for i in range(0,3,1):
+    print('i¬O',i,end=' ')
---
+++ week05/week05-3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    for (int i=0;i<9;i++)
+    {
+        printf("i是%d ",i);///基礎型
+    }
+    printf("\n");
+    for (int i=1;i<=9;i++)
+    {
+        printf("i是%d ",i);///進階型
+    }
+    printf("\n");
+    for(int i=9-1;i>=0;i--)
+    {
+        printf("i是%d ",i);
+    }
+
+}

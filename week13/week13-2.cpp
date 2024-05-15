b84ed974062c5468643312da5d163eb19849f79e
xinxinchu <144758431+xinxinchu@users.noreply.github.com>
2024-05-15 12:02:04 +0800

    week13

---
+++ week13/week13-2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+	int a[10][10],b[10][10],c[10][10];
+	int i,j,k,n;
+	//yourcode
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			scanf("%d",&a[i][j] );
+		}
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			printf("%3d ",c[i][j] );
+		}
+		printf("\n");
+	}
+
+
+}

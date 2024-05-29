9d25b64c38cc53efb98f427c789939608170b514
xinxinchu <144758431+xinxinchu@users.noreply.github.com>
2024-05-29 12:07:10 +0800

    Add files via upload

---
+++ week15/week15-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        N = len(s)
+        for i in range(N):
+            print( s[i],t[i])
+        return 0
---
+++ week15/week15-5a.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int ans = 0;
+	for(int i=1;i<=n;i++)
+	{
+		if(i*i==n)
+		{
+			ans = i;
+			break;
+		}
+
+	}
+	printf("%d",ans);
+}
